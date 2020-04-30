2c81d4b8b9448b6698f5409042dea5ce17a12742
Paul Lunenburg <paul.l.luenburg@gmail.com>
2020-04-30 18:46:03 +1200

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title> PHP Store</title>
+ </head>
+ <body>
+  <?php
+  //Using two forward slashes we creat comments in php.
+  // Within the php tag, we insert values into our html.
+  echo "<h1>Welcome!</h1>";
+  ?>
+ </body>
+</html>
